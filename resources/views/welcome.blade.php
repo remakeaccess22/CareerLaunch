ae2886d3e04f018bbbf783ffb26bf55f4d297b39
remakeaccess22 <alayacyacjude@gmail.com>
2024-08-27 12:28:20 +0800

    On going design and components

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <section>
+        <x-section-heading>Top Jobs</x-section-heading>
+        <div class="grid lg:grid-cols-3 gap-8 mt-6">
+            <x-job-card />
+            <x-job-card />
+            <x-job-card />
+        </div>
+    </section>
+</x-layout>

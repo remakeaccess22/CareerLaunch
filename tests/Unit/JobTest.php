d217b4fb3d435b698052a623491dd22633e19a69
remakeaccess22 <alayacyacjude@gmail.com>
2024-09-12 22:32:47 +0800

    Testing PEST

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,5 +1,16 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+use App\Models\Employer;
+use App\Models\Job;
+
+it('belongs to the employer', function () {
+    //Arrange
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    //Act and Assert
+    expect($job->employer->is($employer))->toBeTrue();
+
 });

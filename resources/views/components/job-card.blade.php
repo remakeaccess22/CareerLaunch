0941bf6a5d637f6215da2f9481815a3867c7385b
remakeaccess22 <alayacyacjude@gmail.com>
2024-10-02 23:34:04 +0800

    job-card resolve

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -5,7 +5,7 @@
             {{ $job->employer->name }}
         </h3>
         <p class="text-sm mt-4">
-            Full Time - From $60,000
+            {{ $job->salary }}
         </p>
     </div>
     <div class="flex justify-between items-center mt-auto">
@@ -13,6 +13,9 @@
             <x-tag size="small">Backend</x-tag>
             <x-tag size="small">Frontend</x-tag>
             <x-tag size="small">Manager</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag :$tag size="small" />
+            @endforeach
         </div>
         <x-employer-logo :width="42" />
     </div>
